0c8f7ff594f342dd6f21113de3456540f1901218
Konstantin Ermakov <bestwick91@gmail.com>
2016-09-17 14:34:56 +0300

    add ChatMessage

--- app/src/main/java/com/roadway/capslabs/roadway_chat/activity/FeedActivity.java
+++ app/src/main/java/com/roadway/capslabs/roadway_chat/activity/FeedActivity.java
@@ -1,18 +1,96 @@
 package com.roadway.capslabs.roadway_chat.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.roadway.capslabs.roadway_chat.ChatMessage;
 import com.roadway.capslabs.roadway_chat.R;
+import com.roadway.capslabs.roadway_chat.adapters.SingleDialogAdapter;
 import com.vk.sdk.VKSdk;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class FeedActivity extends AppCompatActivity {
+
+    Button send;
+    EditText text;
+    ListView listView;
+
+    ArrayList<ChatMessage> chatMessagesList= new ArrayList<>();
+
+    SingleDialogAdapter singleDialogAdapter;
+
+    private class DownloadingMessages extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            String buf = "buf";
+
+            return buf;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // might want to change "executed" for the returned string passed
+            // into onPostExecute() but that is upto you
+            send.setText("Отправить");
+            send.setClickable(true);
+            Collections.reverse(chatMessagesList);
+            singleDialogAdapter.copyArrayList(chatMessagesList);
+            singleDialogAdapter.notifyDataSetChanged();
+            chatMessagesList.clear();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            send.setClickable(false);
+            send.setText("Загрузка");
+            listView.setAdapter(singleDialogAdapter);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_feed);
+        setContentView(R.layout.single_chat);
+
+        text = (EditText) findViewById(R.id.textmsg);
+        listView = (ListView) findViewById(R.id.listmsg);
+        send = (Button) findViewById(R.id.sendmsg);
+
+        singleDialogAdapter = new SingleDialogAdapter(this);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Message Send !",
+                        Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+
+                String msg = text.getText().toString();
+
+                Log.d("msg",msg);
+
+                ChatMessage chatMessage = new ChatMessage(msg, true, null);
+
+                singleDialogAdapter.add(chatMessage);
+                singleDialogAdapter.notifyDataSetChanged();
+            }
+        });
 
         VKSdk.initialize(this);
     }
+
 }
--- app/src/main/java/com/roadway/capslabs/roadway_chat/adapters/SingleDialogAdapter.java
+++ app/src/main/java/com/roadway/capslabs/roadway_chat/adapters/SingleDialogAdapter.java
@@ -1,30 +1,89 @@
 package com.roadway.capslabs.roadway_chat.adapters;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.roadway.capslabs.roadway_chat.ChatMessage;
+import com.roadway.capslabs.roadway_chat.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by konstantin on 15.09.16.
  */
 public class SingleDialogAdapter extends BaseAdapter {
+    private ArrayList<String> inList, outList;
+    private ArrayList<ChatMessage> chatMessagesList = new ArrayList<>();
+    private TextView textView;
+    private Context context;
+
+
+    public SingleDialogAdapter(Context context) {
+        this.context = context;
+    }
+
+
+
+    public void add(ChatMessage obj){
+        chatMessagesList.add(0, obj);
+    }
+
+    public void copyArrayList(ArrayList<ChatMessage> list){
+        chatMessagesList = new ArrayList<>(list);
+    }
+
+    public void addArrayList(ArrayList<ChatMessage> list){
+        chatMessagesList.addAll(0,list);
+    }
+
+    public ArrayList<ChatMessage> getArrayList(){
+        return chatMessagesList;
+    }
+
+    public ChatMessage getMessage(int position){
+        return chatMessagesList.get(position - 1);
+    }
+
+    public void deleteMessage(int position){
+        chatMessagesList.remove(position);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return chatMessagesList.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public ChatMessage getItem(int position) {
+        return this.chatMessagesList.get( getCount() - position - 1);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        ChatMessage chatMessageObj = getItem(position);
+        View row;
+        if (chatMessageObj.getOut()) {
+            row = inflater.inflate(R.layout.dialog_out, null);
+        } else {
+            row = inflater.inflate(R.layout.dialog_in, null);
+        }
+
+        textView = (TextView) row.findViewById(R.id.msg);
+        textView.setText(chatMessageObj.getMsg());
+
+        return row;
     }
+
 }
