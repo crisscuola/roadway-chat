bc68e487cc2ae2db7077df664db2bbed65f71b2c
kirill-m <kmatveev15@gmail.com>
2016-10-04 19:49:28 +0300

    consts

--- app/src/main/java/com/roadway/capslabs/roadway_chat/url/UrlConst.java
+++ app/src/main/java/com/roadway/capslabs/roadway_chat/url/UrlConst.java
@@ -7,14 +7,22 @@ public class UrlConst {
     private UrlConst() {
     }
 
-    public final static String URL = "p30710.lab1.stud.tech-mail.ru";
+    public final static String URL = "p30700.lab1.stud.tech-mail.ru/";
     public final static String URL_CENTRIFUGE = "p30720.lab1.stud.tech-mail.ru";
     public final static String PATH_REGISTER = "register";
+    public final static String PATH_CONFIRM = "confirm";
     public final static String PATH_TOKEN = "token";
     public final static String PATH_CHAT = "chat";
     public final static String PATH_VK = "vk-oauth2";
     public final static String PATH_PARAMETERS = "parameters";
     public final static String PATH_GET_TOKEN = "get_token";
     public final static String PATH_LOGIN = "login";
+    public final static String PATH_LOGOUT = "logout";
+    public final static String PATH_EVENT = "event";
+    public final static String PATH_CREATE = "create";
+    public final static String PATH_GET = "create";
+    public final static String PATH_ALL = "all";
+    public final static String PATH_PROFILE = "profile";
+    public final static String PATH_UPDATE = "update";
     public final static String PATH_API = "api";
 }
